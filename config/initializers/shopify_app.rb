9b7934eb8e892a207dc13ce726165ad6d015dca1
Paul Sherer <wolfymaster@hotmail.com>
2016-07-28 20:54:29 -0400

    Init Commit

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+
+  config.api_key = "4f38225a0f432072a14495a41d331aa7"
+  config.secret = "750c3006c56bed25511f35b931008d66"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
